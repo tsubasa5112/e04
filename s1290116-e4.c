011e237225fd98b6985cef9f9c09f518d19d8be1
佐藤翼 <satoutsubasa@satoutsubasanoMacBook-Pro.local>
2022-07-10 15:13:36 +0900

    User name added

--- s1290116-e4.c
+++ s1290116-e4.c
@@ -5,10 +5,14 @@
 int main() {
   int i;
   int countH = 0, countT = 0;
+  char name[20];
 
   srand(time(NULL));
 
   printf("Tossing a coin...\n");
+  printf("Who are you?\n");
+  scanf("%s", name);
+  printf("Hello, %s!\n", name);
   for (i = 0; i < 3; i++) {
     printf("Round %d: ", i + 1);
     if (rand() % 2 == 1) {
